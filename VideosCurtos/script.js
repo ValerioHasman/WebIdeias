adc29ff759147804cdc4b144a7c000ea0340f5b5
Valério Hasman <valeriopro22@gmail.com>
2024-09-23 12:16:24 -0300

    Melhorias

--- VideosCurtos/script.js
+++ VideosCurtos/script.js
@@ -1,42 +1,38 @@
 const videos = document.getElementsByTagName("video");
 const conteudovps = document.getElementsByClassName("conteudovp");
 
+// define controles
 for (let conteudovp of conteudovps) {
   const video = conteudovp.querySelector("video");
-  video.addEventListener("contextmenu", (event) => {
-    event.preventDefault();
-  });
-  video.addEventListener("click", (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    if (video.paused) {
-      video.play();
-    } else {
-      video.pause();
-    }
-  });
-  conteudovp.addEventListener("scrollend", pausarVideoRodando);
+  video.addEventListener("contextmenu", (event) => { event.preventDefault(); });
+  video.addEventListener("play", rodarVideoVigente);
+  video.addEventListener("click", playPause);
+  video.addEventListener("touchend", playPause);
+  conteudovp.addEventListener("scrollend", rodarVideoVigente);
 }
 
 const tela = document.querySelector(".tela");
-tela.addEventListener("scrollend", pausarVideoRodando);
-
-function pausarVideoRodando() {
-  for (let video of videos) {
-    if (!video.paused) {
-      video.pause();
-      break;
-    }
-  }
-  rodarVideoVigente();
-}
+tela.addEventListener("scrollend", rodarVideoVigente);
 
 function rodarVideoVigente() {
+  console.log("rodarVideoVigente");
   for (let video of videos) {
     const posicoesVideo = video.getBoundingClientRect();
     if ((posicoesVideo.x == 0) && (posicoesVideo.y == 0)) {
       video.play();
-      break;
+    } else {
+      video.pause();
     }
+  }
+}
+
+/**
+* @param {MouseEvent} event 
+* @param {HTMLVideoElement} event.target  */
+function playPause({ target: video }) {
+  if (video.paused) {
+    video.play()
+  } else {
+    video.pause();
   }
 }
