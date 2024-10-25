c4c6d0a5945edc57b7e563af6c266a47cb1398f6
Valério Hasman <valeriopro22@gmail.com>
2024-10-25 19:58:29 -0300

    navegação com CSS puro

---
+++ Navegacao/script.js
@@ -0,0 +1,14 @@
+// const inicio = history.length;
+// let ultimoNivel = inicio;
+
+// function verificaRetorno() {
+
+//   console.log(history.length);
+//   if (ultimoNivel == history.length) {
+//     console.log(ultimoNivel, history.length);
+//     history.go(-1 * (history.length - (inicio - 1)));
+//   }
+//   ultimoNivel = history.length;
+// }
+
+// window.addEventListener("popstate", verificaRetorno);
