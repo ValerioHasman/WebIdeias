65230fe72f166ea0a17d8ebcd059d36f4fd1b92c
Valério Hasman <valeriopro22@gmail.com>
2025-02-08 09:18:40 -0300

    Efeito luminoso e melhoria dos áudios

--- PopK-e/componentes/Botao.js
+++ PopK-e/componentes/Botao.js
@@ -1,18 +1,21 @@
 import _ from "../../Reactive.js";
 
 export default function Botao(prop) {
-  return _.button({
+  const button = _.button({
     className: "botao btn btn-primary border-0 col-auto shadow",
-    style: { backgroundColor: rgb() },
-    onmousedown: RandSom
+    ontouchstart: RandSom
   });
+  button.style.setProperty('--bs-btn-bg', rgb());
+  button.style.setProperty('--bs-btn-bg', rgb());
+  button.style.setProperty('--bs-btn-bg', rgb());
+  return button;
 }
 
 function rgb() {
-  return `rgb(${RandNum()
-    },${RandNum()
-    },${RandNum()
-    })`;
+  return `#${RandNum().toString(16)
+    }${RandNum().toString(16)
+    }${RandNum().toString(16)
+    }`;
 }
 
 function RandNum(inicio = 100, fim = 200) {
@@ -23,7 +26,14 @@ function RandNum(inicio = 100, fim = 200) {
   return numrand;
 }
 
+const audios = [
+  new Audio(`./midias/pop-1.mp3`),
+  new Audio(`./midias/pop-2.mp3`),
+  new Audio(`./midias/pop-3.mp3`),
+  new Audio(`./midias/pop-4.mp3`),
+  new Audio(`./midias/pop-5.mp3`)
+]
+
 function RandSom() {
-  const audio = new Audio(`./midias/pop-${RandNum(1, 5)}.mp3`);
-  audio.play();
+  audios[RandNum(0,4)].play();
 }
