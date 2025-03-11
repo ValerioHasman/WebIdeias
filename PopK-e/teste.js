c8a5464b4ff8540399438f0708bf712bac7a906a
Valério Hasman <valeriopro22@gmail.com>
2025-03-11 19:48:37 -0300

    teste

--- PopK-e/teste.js
+++ PopK-e/teste.js
@@ -2,5 +2,10 @@
   let scripttedin = document.createElement('script');
   scripttedin.src = "https://cdn.jsdelivr.net/npm/eruda";
   document.body.append(scripttedin);
-  scripttedin.onload = function () { eruda.init(); }
+  scripttedin.onload = function () { 
+    eruda.init();
+    if(navigator.userAgent.toLowerCase().includes("tv")){
+      eruda.show();
+    }
+  }
 })();
